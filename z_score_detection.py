e4b45ab86a2d5811bd1ac7c5c240f9e1a9a28174
igorfstefanski <i.stefanski.00@gmail.com>
2022-04-23 21:02:29 +0200

    Add documentation

--- z_score_detection.py
+++ z_score_detection.py
@@ -1,9 +1,26 @@
+"""
+TITLE: OUTLIER DETECTION USING Z SCORE
+AUTHOR: Igor Stefański
+DATE: April 2022
+DESCRIPTION:
+Simple outlier detection in two-column dataframe using Z Score.
+Function treats a dataframe as a whole and counts mean and std for all values, without column division.
+"""
+
+
 import numpy as np
 import pandas as pd
 
+
 def outliers_detector(dataframe):
+    '''
+    Returns list of tuples showing 0-based indexes of a row and column of an outliers.
+    Args:
+        dataframe: dataframe with values
+    Returns: list of tuples with row and column index of an outlier
+    '''
     outliers_ls = []
-    threshold = 3
+    THRESHOLD = 3
 
     mean = dataframe.values.mean()
     std = dataframe.values.std(ddof=1)
@@ -16,7 +33,7 @@ def outliers_detector(dataframe):
             else:
                 z = 1
 
-            if z > threshold:
+            if z > THRESHOLD:
                 temp = (counter, dataframe.columns.get_loc(i))
                 outliers_ls.append(temp)
 
@@ -26,6 +43,7 @@ def outliers_detector(dataframe):
 
 
 if __name__ == __main__:
+    # example data
     data = {'X1': [155, 15, 13, 11, 12, 11, 15, 156, 13, 10, 12],
             'X2': [149, 169, 172, 148, 156, 162, 189, 555, 147, 150, 153]}
 
